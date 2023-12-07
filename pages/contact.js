c1585cec89082ed693bb68a2f3546aeabac58a40
Oleksii <95025971+Pervyy777@users.noreply.github.com>
2023-12-07 20:07:21 +0100

    better NavBar

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
-
+import Link from 'next/link';
 const Contact = () => {
     return (
         <Layout>
-                <h1 className={styles.text}>Contact Us</h1>
-                <p className={styles.text}>This is the contact page.</p>
+            <div className={styles.text}>
+                <div>
+                    <h1>Contact Us</h1>
+                    <p>Tg: <Link href="https://t.me/Werds0777" target="_blank">@Werds0777</Link></p>
+                    <p>Email: werds0777@gmail.com</p>
+                </div>
+                <div>
+                    <h1>Follow Us</h1>
+                    <Link href="https://t.me/+26q4Tlt42yM0OTYy" target="_blank"><p>Telegram</p></Link>
+                    <Link href="" target="_blank">Discord</Link>
+                </div>
+            </div>
         </Layout>
     );
 };
